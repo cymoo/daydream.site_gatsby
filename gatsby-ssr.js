3deffe43d44d90ae05082e91ede9bfc0e4615b3f
cymoo <milkywayjc@hotmail.com>
2020-10-30 23:12:42 +0800

    add page analytics

---
+++ gatsby-ssr.js
@@ -0,0 +1,23 @@
+const React = require("react")
+
+// NOTE: Google analytics is better but the service is unavailable in China.
+exports.onRenderBody = ({ setHeadComponents }) => {
+  if (process.env.NODE_ENV === "production") {
+    setHeadComponents([
+      <script
+        key="baidu-analytics-script"
+        dangerouslySetInnerHTML={{
+          __html: `
+          var _hmt = _hmt || [];
+          (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?b88d1fbf9df07f57aba61feb1728a9f9";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+            })();
+          `,
+        }}
+      />,
+    ])
+  }
+}
