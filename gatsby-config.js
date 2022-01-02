56bbe39548d5beb676074010666da4788f949a52
cymoo <milkywayjc@hotmail.com>
2022-01-02 14:49:06 +0800

    bugfix: convert to relative image url to absolute url

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,11 +36,17 @@ module.exports = {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
               return allMarkdownRemark.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
-                  // excerpt: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
+                  custom_elements: [
+                    {
+                      "content:encoded": edge.node.html.replace(
+                        /(?<="|\s)\/static\//g,
+                        `${site.siteMetadata.siteUrl}\/static\/`
+                      ),
+                    },
+                  ],
                 })
               })
             },
